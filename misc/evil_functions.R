fd8c88358e3d908322cf181d76b9301d14c3ceae
Michael Culshaw-Maurer <mjculshawmaurer@ucdavis.edu>
2021-05-25 13:13:00 -0700

    added some evil

---
+++ misc/evil_functions.R
@@ -0,0 +1,39 @@
+# packages ----------------------------------------------------------------
+
+
+# description -------------------------------------------------------------
+# absolutely evil stuff to put in someone's .Rprofile
+
+# example -----------------------------------------------------------------
+
+attach(list(
+  `+` = function(a, b){
+    noquote(paste0(sum(a,b,rnorm(1,0,0.0001)), "ish"))
+  },
+  
+  `(` = function(a){
+    a*rnorm(1,1,0.001)
+  },
+  
+  `<-` = function(a,b){
+    warning("lol nah")
+  },
+  
+  `/` = function(a,b){
+    warning("divided we fall, truly words to live by")
+  },
+  
+  library = function(package){
+    warning("you have several overdue books and your library card has been revoked")
+  }
+), name = "JokeFunctions", warn.conflicts = FALSE)
+
+(2)
+
+x <- 3
+
+2 + 2
+
+4 / 5
+
+library(tidyverse)
