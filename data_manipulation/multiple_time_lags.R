08f5973654e0e1c539422b78207b0105a13b9d64
Michael Culshaw-Maurer <culshawmaurer@email.arizona.edu>
2022-02-07 16:57:17 -0600

    added lag corr function

--- data_manipulation/multiple_time_lags.R
+++ data_manipulation/multiple_time_lags.R
@@ -27,3 +27,32 @@ get_lags <- function(.data, ..., lags = 1:5, by, time){
 storms2 %>% 
   get_lags(wind, pressure, lags = 1:4, by = storm_id, time = date) %>% 
   select(storm_id, date, starts_with("wind"), starts_with("pressure"))
+
+# this function will get correlations between the original variable and every lag OR between a comp_var and every lag + the original
+
+make_lag_corr <- function(.data, ..., lags = 1:5, by, time, comp_var){
+  x <- .data %>% 
+    select(!!!ensyms(...), !!ensym(by), !!ensym(time), !!ensym(comp_var)) %>% 
+    get_lags(... = ..., lags = lags, by = !!ensym(by), time = !!ensym(time)) %>% 
+    select(-!!ensym(by), -!!ensym(time)) %>% 
+    relocate(!!ensym(comp_var)) %>% 
+    cor(use = "pairwise.complete.obs") %>% 
+    as.data.frame() %>% 
+    select(1) %>% 
+    rename_with(~paste0(.x, "_corr")) 
+  
+  if(nrow(x) > length(lags)+1){
+    x %>% 
+      mutate(lag = c(NA, 0, lags)) %>% 
+      .[-1,]
+  } else{
+    x %>% 
+    mutate(lag = c(0,lags))
+  }
+}
+
+storms2 %>% 
+  make_lag_corr(wind, lags = 1:4, by = storm_id, time = date, comp_var = pressure)
+
+storms2 %>% 
+  make_lag_corr(wind, lags = 1:4, by = storm_id, time = date)
